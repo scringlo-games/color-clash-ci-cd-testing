24cdb6ce1d0dafc1daa68473ad9d8e5b15aeb6ba
Travis French <travis.french.1991@gmail.com>
2024-01-30 22:53:06 -0800

    Renamed method signatures and updated documentation.

--- Assets/Runtime/Conditions/Condition.cs
+++ Assets/Runtime/Conditions/Condition.cs
@@ -1,25 +1,49 @@
 ﻿namespace ScringloGames.ColorClash.Runtime.Conditions
 {
+    /// <summary>
+    /// Responsible for encapsulating behaviour that is common between all conditions and for defining a signature
+    /// that can be used to treat conditions in the same way by certain systems (such as ConditionBank). Inheritors
+    /// should override the OnApplied, OnTicked, and OnExpired methods in order to define condition-specific behavior.
+    /// </summary>
     public abstract class Condition
     {
-        public bool IsTicking { get; private set; }
+        /// <summary>
+        /// Determines the duration for which the condition should continue to be active.
+        /// </summary>
         public float Duration { get; set; }
+        /// <summary>
+        /// The remaining time for which the condition will continue to be active. This will be initialized at Duration
+        /// and decremented every frame. When this value reaches zero, the condition will be automatically removed.
+        /// </summary>
         public float Time { get; protected set; }
-
+        
         protected Condition()
         {
             this.Time = this.Duration;
         }
         
+        /// <summary>
+        /// Invoked when the condition is first applied to an entity.
+        /// </summary>
+        /// <param name="bank">The ConditionBank that invokes this callback.</param>
         public virtual void OnApplied(ConditionBank bank)
         {
         }
-
+        
+        /// <summary>
+        /// Invoked every "tick", as determined by the settings defined in the ConditionBank.
+        /// </summary>
+        /// <param name="bank">The ConditionBank that invokes this callback.</param>
+        /// <param name="deltaTime">The interval in seconds from the last frame to the current one (Read Only).</param>
         public virtual void OnTicked(ConditionBank bank, float deltaTime)
         {
         }
 
-        public virtual void OnRemoved(ConditionBank bank)
+        /// <summary>
+        /// Invoked when the condition is removed from an entity.
+        /// </summary>
+        /// <param name="bank">The ConditionBank that invokes this callback.</param>
+        public virtual void OnExpired(ConditionBank bank)
         {
         }
     }
--- Assets/Runtime/Conditions/ConditionBank.cs
+++ Assets/Runtime/Conditions/ConditionBank.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 
 namespace ScringloGames.ColorClash.Runtime.Conditions
 {
+    /// <summary>
+    /// Responsible for managing a set of conditions on a specific entity. This class will maintain a list of
+    /// conditions that the entity has, execute the abstracted logic of those conditions, decrement the time of those
+    /// conditions and automatically remove them when expired.
+    /// </summary>
     public class ConditionBank : MonoBehaviour
     {
+        /// <summary>
+        /// Applies the specified condition to this entity.
+        /// </summary>
+        /// <param name="condition">The condition to be applied.</param>
+        /// <exception cref="NotImplementedException"></exception>
         public void Apply(Condition condition)
         {
             throw new NotImplementedException();
