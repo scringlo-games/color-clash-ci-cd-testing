e902221f447e858d937f71f11cc15f9ddb1d5050
Travis French <travis.french.1991@gmail.com>
2024-01-30 22:18:25 -0800

    Merged prototype of feature/COLOR-54.

---
+++ Assets/Runtime/Conditions/Condition.cs
@@ -0,0 +1,26 @@
+﻿namespace ScringloGames.ColorClash.Runtime.Conditions
+{
+    public abstract class Condition
+    {
+        public bool IsTicking { get; private set; }
+        public float Duration { get; set; }
+        public float Time { get; protected set; }
+
+        protected Condition()
+        {
+            this.Time = this.Duration;
+        }
+        
+        public virtual void OnApplied(ConditionBank bank)
+        {
+        }
+
+        public virtual void OnTicked(ConditionBank bank, float deltaTime)
+        {
+        }
+
+        public virtual void OnRemoved(ConditionBank bank)
+        {
+        }
+    }
+}
---
+++ Assets/Runtime/Conditions/ConditionBank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Conditions
+{
+    public class ConditionBank : MonoBehaviour
+    {
+        public void Apply(Condition condition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
