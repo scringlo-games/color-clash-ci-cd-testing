2652ef2aa0fb42a85fb0f0e9748fa913329784ce
Travis French <travis.french.1991@gmail.com>
2024-01-31 15:55:10 -0800

    Performed project housekeeping.

---
+++ Assets/Runtime/Shared/ProjectileHitScript.cs
@@ -0,0 +1,24 @@
+using ScringloGames.ColorClash.Runtime.Health;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Shared
+{
+    public class ProjectileHitScript : MonoBehaviour
+    {
+        [SerializeField] private new string tag;
+        [SerializeField] private int damage;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag(this.tag))
+            {
+                var healthHandler = collision.collider.GetComponent<HealthHandler>();
+                if (healthHandler != null)
+                {
+                    healthHandler.TakeDamage(this.damage);
+                }
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Runtime/Shared/ScreenSpaceFollow.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Shared
+{
+    public class ScreenSpaceFollow : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform objectToFollow; 
+        [SerializeField]
+        private Camera mainCamera;
+        private RectTransform thisObj;
+        [SerializeField]
+        private float offsetX;
+        [SerializeField]
+        private float offsetY;
+
+        private void Start()
+        {
+            //assigns the RectTransform component on 'this' to the 'thisObj' variable
+            this.thisObj = this.GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            //finds the screen pos of 'Object To Follow', then sets the position of 'this' to that screen position
+            var screenPos = this.mainCamera.WorldToScreenPoint(this.objectToFollow.position);
+            screenPos.y += this.offsetY;
+            screenPos.x += this.offsetX;
+            this.thisObj.position = screenPos;
+        }
+    }
+}
