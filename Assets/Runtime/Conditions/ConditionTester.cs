68ffb3a987b7e795183b758a5f6f7efe4ef0e6e6
Travis French <travis.french.1991@gmail.com>
2024-01-31 21:15:47 -0800

    Merged feature/COLOR-54.

---
+++ Assets/Runtime/Conditions/ConditionTester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Conditions
+{
+    public class ConditionTester : MonoBehaviour
+    {
+        private void Start()
+        {
+            var condition = new MyCondition()
+            {
+                Duration = 10f,
+            };
+            var bank = this.GetComponent<ConditionBank>();
+            
+            bank.Apply(condition);
+        }
+
+        public class MyCondition : Condition
+        {
+            public override void OnTicked(ConditionBank bank, float deltaTime)
+            {
+                Debug.Log($"Ticked!");
+            }
+        }
+    }
+}
