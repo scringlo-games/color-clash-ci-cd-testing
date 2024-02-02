743dcd3561ecfa7b04be490b721a6c6769ed9dd7
Travis French <travis.french.1991@gmail.com>
2024-02-02 15:57:47 -0800

    Implemented weapon swap HUD.

--- Assets/Runtime/UI/WeaponSwapHUD.cs
+++ Assets/Runtime/UI/WeaponSwapHUD.cs
@@ -18,10 +18,39 @@ namespace ScringloGames.ColorClash.Runtime.UI
 
         private void OnEnable()
         {
+            this.swapper.Swapped += this.OnSwapped;
         }
 
         private void OnDisable()
         {
+            this.swapper.Swapped -= this.OnSwapped;
+        }
+        
+        private void OnSwapped()
+        {
+            switch (this.swapper.CurrentIndex)
+            {
+                case 0:
+                    this.redDarkener.gameObject.SetActive(false);
+                    this.blueDarkener.gameObject.SetActive(true);
+                    this.yellowDarkener.gameObject.SetActive(true);
+                    break;
+                case 1:
+                    this.redDarkener.gameObject.SetActive(true);
+                    this.blueDarkener.gameObject.SetActive(false);
+                    this.yellowDarkener.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    this.redDarkener.gameObject.SetActive(true);
+                    this.blueDarkener.gameObject.SetActive(true);
+                    this.yellowDarkener.gameObject.SetActive(false);
+                    break;
+                default:
+                    this.redDarkener.gameObject.SetActive(true);
+                    this.blueDarkener.gameObject.SetActive(true);
+                    this.yellowDarkener.gameObject.SetActive(true);
+                    break;
+            }
         }
     }
 }
