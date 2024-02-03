8d47013ffd1565381bcf2abc74b2f85199c47095
Travis French <travis.french.1991@gmail.com>
2024-02-02 17:01:23 -0800

    Made it so that enemies can actually die when being reduced to zero health.

--- Assets/Runtime/Death/DeathHandler.cs
+++ Assets/Runtime/Death/DeathHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using ScringloGames.ColorClash.Runtime.Health;
 using UnityEngine;
 
@@ -7,9 +8,23 @@ namespace ScringloGames.ColorClash.Runtime.Death
     public class DeathHandler : MonoBehaviour
     {
         [SerializeField]
+        private float destroyDelay = 0.25f;
+        [SerializeField]
         private HealthHandler healthHandler;
         public event Action Killed;
 
+        public void Kill()
+        {
+            this.Killed?.Invoke();
+            this.StartCoroutine(this.DestroyAfterDelay());
+        }
+
+        private IEnumerator DestroyAfterDelay()
+        {
+            yield return new WaitForSeconds(this.destroyDelay);
+            Destroy(this.gameObject);
+        }
+        
         private void OnEnable()
         {
             this.healthHandler.HealthChanged += this.OnHealthChanged;
@@ -19,13 +34,13 @@ namespace ScringloGames.ColorClash.Runtime.Death
         {
             this.healthHandler.HealthChanged -= this.OnHealthChanged;
         }
-    
+
         //Checks if health is below or equal to zero, then invokes the 'OnKilled' event if the condition is met.
         private void OnHealthChanged(int health)
         {
             if (health <= 0)
             {
-                this.Killed?.Invoke();
+                this.Kill();
             }
         }
     }
