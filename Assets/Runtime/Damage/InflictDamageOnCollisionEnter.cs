b600c56caaff5ba40545246e847d0dcd40bbdaa2
Travis French <travis.french.1991@gmail.com>
2024-02-03 15:16:56 -0800

    Made enemies actually hurt the player on contact.

---
+++ Assets/Runtime/Damage/InflictDamageOnCollisionEnter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ScringloGames.ColorClash.Runtime.Health;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Damage
+{
+    public class InflictDamageOnCollisionEnter : MonoBehaviour
+    {
+        [SerializeField]
+        private string tagFilter;
+        [SerializeField]
+        private int damageToInflict;
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var other = collision.collider;
+            var healthHandler = other.GetComponent<HealthHandler>();
+
+            if (healthHandler == null)
+            {
+                return;
+            }
+
+            if (!other.CompareTag(this.tagFilter))
+            {
+                return;
+            }
+
+            healthHandler.TakeDamage(this.damageToInflict);
+        }
+    }
+}
