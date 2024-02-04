dfc29380c22e6e6638dcd9b3d2e20db9c1a478df
Travis French <travis.french.1991@gmail.com>
2024-02-04 15:04:32 -0800

    Fixed projectile chaining.

---
+++ Assets/Runtime/Conditions/ApplyAOEConditionToOtherOnCollisionEnter.cs
@@ -0,0 +1,13 @@
+﻿namespace ScringloGames.ColorClash.Runtime.Conditions
+{
+    public class ApplyAOEConditionToOtherOnCollisionEnter : ApplyConditionToOtherOnCollisionEnter
+    {
+        protected override Condition GetCondition()
+        {
+            return new AOECondition()
+            {
+                Duration = 2f,
+            };
+        }
+    }
+}
