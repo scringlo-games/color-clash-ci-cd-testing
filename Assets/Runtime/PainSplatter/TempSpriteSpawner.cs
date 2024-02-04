a59fa5373e6abd8dfd2968978d79361381f53e2c
kyoninh <132638948+kyoninh@users.noreply.github.com>
2024-02-04 14:22:59 -0800

    updated pain project prefabs + added new paint splatter sprite

--- Assets/Runtime/PainSplatter/TempSpriteSpawner.cs
+++ Assets/Runtime/PainSplatter/TempSpriteSpawner.cs
@@ -12,13 +12,19 @@ namespace ScringloGames.ColorClash.Runtime
     public class TempSpriteSpawner : MonoBehaviour
     {
     
-        private Vector3 angle = Random.rotation.eulerAngles;
+        private Vector3 angle;
         [SerializeField] private GameObject spawnObj;
-        private int spriteCount;
+        private static int spriteCount;
+
+        void OnDestroy()
+        {
+            CreateNewSprite(this.transform.position);
+        }
         public void CreateNewSprite(UnityEngine.Vector3 targetPos)
         {
             /*creates a new instance of the given prefab, then finds the spriterenderer component on the prefab and increments the sorting 
             order by one, ensuring that paint decals are rendered in the correct order. */
+            angle = new UnityEngine.Vector3 (0f,0f, Random.Range(-90f, 90f));
             GameObject newObj = Instantiate(spawnObj,targetPos, Quaternion.Euler(angle));
             newObj.GetComponent<SpriteRenderer>().sortingOrder = spriteCount++;
             Debug.Log($"decal pos: {newObj.transform.position}");
